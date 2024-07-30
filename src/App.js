9f19c499478f85550f4c95b61e44facd54f845fb
EdvinBinakaj <ebi@rasiah.de>
2024-07-30 14:58:08 +0200

    add-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="1001.1.jpg" width="500" height="600" alt="abc" />
+    </div>
+  );
+}
+
+export default App;
